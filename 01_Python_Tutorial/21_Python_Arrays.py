d2e49d06d62fb2fdca21e699ddf361d8d8af64cd
danielOuattara <daniel.ouattara@gmx.com>
2023-02-01 23:29:00 +0100

    arrays

---
+++ 01_Python_Tutorial/21_Python_Arrays.py
@@ -0,0 +1,20 @@
+# bin/bash !
+# Daniel OUATTARA
+# 25 03 2022
+# 01-Python Tutorial-Python3.x
+# w3schools.com/python/
+
+"""
+Python Arrays
+===============
+
+Note: Python does not have built-in support for Arrays,
+but Python Lists can be used instead.
+
+Arrays
+----------
+Note: This page shows you how to use LISTS as ARRAYS, however,
+to work with arrays in Python you will have to import a library,
+like the NumPy library.
+
+Arrays are used to store multiple values in one single variable:"""
